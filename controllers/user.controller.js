86a607a4e57411d20513d2cee962d606bc8ae5a4
Cesar Pintos <pintos.cesar01@gmail.com>
2021-08-12 20:55:01 -0300

    getUsers añadida

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,38 +1,51 @@
-const User = require('../models/user');
-const bcryptjs = require('bcryptjs');
-const { userValidator } = require('../middleware/userValidator')
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+const { userValidator } = require("../middleware/userValidator");
 
-const getUsers = (req, res) => {
-  res.send('Hola');
+const getUsers = async (req, res) => {
+  let { limit = 100, page = 0 } = req.query;
+  const usuarios = User.find({ active: true })
+    .skip(Number(page * limit))
+    .limit(Number(limit));
+    const total = User.countDocuments({ active: true });
+    const resp = await Promise.all([
+      usuarios,
+      total
+    ])
+  res.status(200).json({
+    usuarios: resp[0],
+    total: resp[1],
+    limit,
+    page,
+  });
 };
 
 const getUserByParamsID = (req, res) => {};
 
 const addUser = async (req, res) => {
-
   // Solo agarrar campos necesarios, ignoramos la basura
-  const { name, email, password, img = 'none' } = req.body;
-  const stagedUser = {name, email, password}
+  const { name, email, password, img = "none" } = req.body;
+  const stagedUser = { name, email, password };
 
   // Validación de errores en la request
-  const errors = await userValidator(stagedUser)
+  const errors = await userValidator(stagedUser);
   if (errors) {
     res.status(400).json({
       ok: false,
       errors,
-      receivedData: { name, email, password, img }
-    })
-    return
+      receivedData: { name, email, password, img },
+    });
+    return;
   }
   // Verificar si el correo existe
   const emailExists = await User.findOne({ email });
 
   if (emailExists) {
     res.status(400).json({
-      code: 'error/email-not-unique',
-      message: 'Este email ya esta registrado.',
+      code: "error/email-not-unique",
+      message: "Este email ya esta registrado.",
     });
-    return
+    return;
   }
   // Encriptación de contraseña
   const salt = bcryptjs.genSaltSync();
